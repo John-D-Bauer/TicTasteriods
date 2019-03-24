68294b1e8da7be961b8ddf1bbfd857e12fda47d1
JohnnydCoder <43066833+JohnnydCoder@users.noreply.github.com>
2019-03-24 16:09:54 -0400

    Add files via upload

---
+++ TicTacToe.ino
@@ -0,0 +1,55 @@
+#include "Arduboy2.h"
+Arduboy2 arduboy;
+
+#include "globals.h"
+#include "gameFunctions.h"
+
+void gameLoop() 
+{
+  
+  arduboy.drawLine(0, 21, 128, 21, WHITE);
+  arduboy.drawLine(0, 42, 128, 42, WHITE);
+  arduboy.drawLine(42, 0, 42, 64, WHITE);
+  arduboy.drawLine(84, 0, 84, 64, WHITE);
+
+  if (playerTurn)
+  {
+  pointerFunction();
+  }
+  
+  XorOFunction();
+
+  if(!playerTurn)
+  {
+    easyAI();
+  }
+
+  checkBoard();
+} 
+
+
+void setup() 
+{
+  arduboy.begin();
+  arduboy.setFrameRate(45);
+  arduboy.display();
+
+  
+  arduboy.clear();
+}
+
+void loop() 
+{
+  if(!(arduboy.nextFrame())) 
+  {
+    return;
+  }
+
+  arduboy.pollButtons();
+
+  arduboy.clear();
+
+  gameLoop();
+
+  arduboy.display();
+}
